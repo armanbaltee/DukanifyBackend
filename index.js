dde0f43120b2ad953452e7e53287f1a4a097046c
Taimoor Anees <shaykhtimur@gmail.com>
2025-08-07 17:03:57 +0500

    taimur

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const session = require('express-session');
-const route = require('./routes/auth/auth.routes')
-const adminRoutes = require('./routes/admin/admin.routes')
-const storeRoutes = require('./routes/store/store.routes')
-const productCategory = require('./routes/product/category.routes')
-const productRoutes = require("./routes/product/product.routes")
-const profileRoutes = require('./routes/profile/profile.routes')
-const Store = require ('./models/store.model')
-const SearchRoutes = require('./routes/search/search.routes')
-const checkoutRoutes = require('./routes/checkout/checkout.routes')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const session = require("express-session");
+const route = require("./routes/auth/auth.routes");
+const adminRoutes = require("./routes/admin/admin.routes");
+const storeRoutes = require("./routes/store/store.routes");
+const productCategory = require("./routes/product/category.routes");
+const productRoutes = require("./routes/product/product.routes");
+const profileRoutes = require("./routes/profile/profile.routes");
+const Store = require("./models/store.model");
+const SearchRoutes = require("./routes/search/search.routes");
+const checkoutRoutes = require("./routes/checkout/checkout.routes");
+const storeOrdersRoutes = require("./routes/store-orders/storeOrders.routes");
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:4200',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
 
+app.use("/api/auth", route);
+app.use("/api/admin", adminRoutes);
+app.use("/api/store", storeRoutes);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/store', storeRoutes)
+app.use("/product/category", productCategory);
+app.use("/product", productRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/uploads", express.static("uploads"));
+app.use("/api/search", SearchRoutes);
+app.use("/checkout", checkoutRoutes);
 
-app.use('/product/category', productCategory);
-app.use('/product', productRoutes)
-app.use('/api/profile', profileRoutes)
-app.use('/uploads', express.static('uploads'));
-app.use('/api/search', searchRoutes);
-app.use('/checkout', checkoutRoutes)
-
-app.use('/storeOrders', storeOrdersRoutes)
-
+app.use("/storeOrders", storeOrdersRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/Dukanify', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, "0.0.0.0", () => console.log(`Server running at http://localhost:${PORT}`));
+mongoose
+  .connect("mongodb://localhost:27017/Dukanify", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, "0.0.0.0", () =>
+      console.log(`Server running at http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
